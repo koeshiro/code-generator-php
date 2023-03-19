a80a62660b99cbeabea8cd959058d427a068f393
koeshiro kagami <koeshiro@yandex.ru>
2023-03-20 00:31:22 +0300

    interface fix

--- koeshiro/CodeGenerator/src/Interfaces/LangTemplateExceptionInterface.php
+++ koeshiro/CodeGenerator/src/Interfaces/LangTemplateExceptionInterface.php
@@ -1,14 +1,13 @@
 <?php
+
 namespace CodeGenerator\Interfaces;
 
 /**
- *
  * @author koesh
  */
 interface LangTemplateExceptionInterface
 {
+    public function __construct(string $message = '', string $lang = 'php', int $code = 0, \Throwable $previous = null);
 
-    public function __construct(string $message = "", string $lang = "php", int $code = 0, \Throwable $previous = NULL);
-
-    public function getLang() : string;
+    public function getLang(): string;
 }
